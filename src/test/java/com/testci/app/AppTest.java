e3f8ea7df699d59ed06cdc14ece1ffcc97f5778f
mainGitPro <apxbox@protonmail.com>
2020-03-18 20:57:24 -0700

    init repo

---
+++ src/test/java/com/testci/app/AppTest.java
@@ -0,0 +1,27 @@
+package com.testci.app;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void shouldReturnTrue(){
+        App a = new App();
+        assertEquals(a.basicSum(5,5) ,10);
+    }
+}
